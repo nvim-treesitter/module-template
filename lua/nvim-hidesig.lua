c03e81db5ef9016d0171fad1a0cb0b6f029b13d7
Si Le <omnisyle@gmail.com>
2022-03-13 23:06:31 -0400

    Adds default init config and rename to hidesig

--- lua/nvim-hidesig.lua
+++ lua/nvim-hidesig.lua
@@ -4,11 +4,12 @@ local M = {}
 
 function M.init()
   require "nvim-treesitter".define_modules {
-    nvim_hidesig = {
+    hidesig = {
       module_path = "nvim-hidesig.internal",
       is_supported = function(lang)
         return queries.get_query(lang, "sig_def") ~= nil
-      end
+      end,
+      opacity = 0.75
     }
   }
 end
