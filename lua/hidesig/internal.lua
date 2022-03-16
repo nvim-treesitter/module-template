3f3a34ca09dac4d3f6a9926f5f792a06929f14d5
Si Le <omnisyle@gmail.com>
2022-03-16 01:09:31 -0400

    Refactor to use vim augroup

--- lua/hidesig/internal.lua
+++ lua/hidesig/internal.lua
@@ -1,8 +1,11 @@
 local highlight_utils = require("hidesig.highlight_utils")
+local ts_query = require("nvim-treesitter.query")
+local ts_utils = require("nvim-treesitter.ts_utils")
 local util = require("hidesig.util")
 
 local hidesig = {}
 local cachedHighlightGroup = {}
+local namespace = vim.api.nvim_create_namespace("hidesig_ns")
 
 ---Get or create new highlight group
 ---@param bufnr integer
@@ -10,7 +13,6 @@ local cachedHighlightGroup = {}
 ---@param startCol integer
 ---@return string # Highlight group name
 function hidesig.getOrCreateHighlightGroup(bufnr, startLine, startCol)
-
   local highlightGroup = highlight_utils.getHightlightGroupForRange(bufnr, startLine, startCol)
 
   if cachedHighlightGroup[highlightGroup] ~= nil then
@@ -18,13 +20,13 @@ function hidesig.getOrCreateHighlightGroup(bufnr, startLine, startCol)
   end
 
   local color = highlight_utils.getHighlightGroupColor(highlightGroup)
-  local darkenValue = hidesig.configs.opacity or 0.75
+  local darkenValue = hidesig.opacity or 0.75
   local newHighlightGroup = string.format("%sDimmed", highlightGroup)
 
   cachedHighlightGroup[highlightGroup] = newHighlightGroup
 
   vim.api.nvim_set_hl(
-    hidesig.ns,
+    namespace,
     newHighlightGroup,
     {
       fg = util.darken(color, darkenValue),
@@ -40,18 +42,10 @@ end
 ---@param bufnr integer Buffer number
 ---@param node any Treesitter node
 function hidesig.traverseNode(bufnr, node)
-  local startLine, startCol, _, endCol = node:range() -- range of the capture
+  local startLine, startCol, _, _ = node:range() -- range of the capture
   local highlightGroup = hidesig.getOrCreateHighlightGroup(bufnr, startLine, startCol)
 
-  vim.api.nvim_buf_add_highlight(
-    bufnr,
-    hidesig.ns,
-    highlightGroup,
-    startLine,
-    startCol,
-    endCol
-  )
-
+  ts_utils.highlight_node(node, bufnr, namespace, highlightGroup)
   if node:child_count() < 1 then
     return
   else
@@ -63,81 +57,79 @@ end
 
 ---Highlight specific lines
 ---@param bufnr integer # Buffer number
----@param changes table # List of changes in format { startRow, startCol, endRow, endCol }
+---@param range table # List of changes in format { startRow, endRow }
 ---@param tree any # Syntax tree
 ---@param lang string # Buffer language
-function hidesig.highlightLines(bufnr, changes, tree, lang)
+function hidesig.highlightLines(bufnr, range, tree, lang)
   -- check if there's a popup visible
   if vim.fn.pumvisible() == 1 or not lang then
     return
   end
 
   local rootNode = tree:root()
-  local query = [[
-    (
-      call
-        method: (identifier) @sig_keyword
-        block:  [(block) (do_block)]
-      (#eq? @sig_keyword "sig")
-    ) @sig_def
-  ]]
-  local parsedQuery = vim.treesitter.parse_query(lang, query)
+  local parsedQuery = ts_query.get_query(lang, "hidesig")
+  local startRow = range[1]
+  local endRow = range[2]
 
-  for _, change in ipairs(changes) do
-    local startRow = change[1]
-    local endRow = change[3]
+  vim.api.nvim_buf_clear_namespace(bufnr, namespace, startRow, endRow)
 
-    vim.api.nvim_buf_clear_namespace(bufnr, hidesig.ns, startRow, endRow)
+  for _, captures in parsedQuery:iter_matches(rootNode, bufnr, startRow, endRow) do
+    local sigBlock = captures[2] -- capture @sig_def
 
-    for _, captures in parsedQuery:iter_matches(rootNode, bufnr, startRow, endRow) do
-      local sigBlock = captures[2] -- capture @sig_def
-
-      if sigBlock ~= nil and not sigBlock:has_error() then
-        for rootChildNode in sigBlock:iter_children() do
-          hidesig.traverseNode(bufnr, rootChildNode)
-        end
+    if sigBlock ~= nil and not sigBlock:has_error() then
+      for rootChildNode in sigBlock:iter_children() do
+        hidesig.traverseNode(bufnr, rootChildNode)
       end
     end
   end
 end
 
---- Setup hidesig module with config options, must be called before perform
----@param configs table configuration for hidesig
--- @param configs.opacity float value from 0.0 to 1.0
-function hidesig.setup(bufnr, configs)
-  hidesig.ns = vim.api.nvim_create_namespace("hidesig_ns")
-  vim.api.nvim__set_hl_ns(hidesig.ns)
-  hidesig.configs = configs or {}
-  hidesig.enabledBuffers = {}
-  hidesig.enabledBuffers[bufnr] = true
-end
-
 --- Teardown state and clear namespace for buffer
 ---@param bufnr integer # Buffer number
 function hidesig.teardown(bufnr)
-  if hidesig.ns == nil then
-    error("[hidesig.lua] Must call setup() before teardown()")
-    return
-  end
-
-  hidesig.enabledBuffers[bufnr] = nil
-  vim.api.nvim_buf_clear_namespace(bufnr, hidesig.ns, 0, -1)
+  vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
 end
 
---- Perform hidesig highlighting logic for Ruby sorbet signature definition
---- @param bufnr integer buffer number
---- @param lang string Language name
-function hidesig.perform(bufnr, lang)
-  if hidesig.ns == nil then
-    error("[hidesig.lua] Must call setup() before perform()")
-    return
-  end
+--- Perform hidesig highlighting logic for Ruby sorbet signature definition for entire buf
+--- @param bufnr integer # Buffer number
+--- @param lang string # Language name
+--- @param opacity number # Number from 0.0 to 1.0
+function hidesig.fullUpdate(bufnr, lang, opacity)
+  hidesig.opacity = opacity
+  vim.api.nvim__set_hl_ns(namespace)
 
   local parser = vim.treesitter.get_parser(bufnr, lang)
   local syntaxTree = parser:parse()[1]
+  local treeRange = { syntaxTree:root():range() }
 
   -- highlight the entire buffer
-  hidesig.highlightLines(bufnr, { { syntaxTree:root():range() } }, syntaxTree, lang)
+  hidesig.highlightLines(
+    bufnr,
+    { treeRange[1], treeRange[3] },
+    syntaxTree,
+    lang
+  )
+end
+
+--- Perform hidesig highlighting logic for Ruby sorbet signature definition for visible part of buf
+--- @param bufnr integer # Buffer number
+--- @param lang string # Language name
+--- @param opacity number # Number from 0.0 to 1.0
+function hidesig.updateVisibleBuf(bufnr, lang, opacity)
+  hidesig.opacity = opacity
+  vim.api.nvim__set_hl_ns(namespace)
+
+  local parser = vim.treesitter.get_parser(bufnr, lang)
+  local syntaxTree = parser:parse()[1]
+  local startLine = vim.fn.line('w0') - 1
+  local endLine = vim.fn.line('w$')
+
+  hidesig.highlightLines(
+    bufnr,
+    { startLine, endLine },
+    syntaxTree,
+    lang
+  )
 end
 
 return hidesig
--- lua/hidesig/main.lua
+++ lua/hidesig/main.lua
@@ -1,16 +1,49 @@
 local hidesig = require("hidesig.internal")
-local configs = require("nvim-treesitter.configs")
+local ts_configs = require("nvim-treesitter.configs")
+local cmd = vim.api.nvim_command
+
+local cmdEvents = {
+  "InsertLeave",
+  "CursorMoved",
+}
 
 local M = {}
 
+local function partialUpdateCmd(event, bufnr, lang, opacity)
+  return string.format(
+    [[autocmd %s <buffer=%d> lua require('hidesig.internal').updateVisibleBuf(%d, '%s', %0.2f)]],
+    event,
+    bufnr,
+    bufnr,
+    lang,
+    opacity
+  )
+end
+
 function M.attach(bufnr, lang)
-  local config = configs.get_module("hidesig")
-  hidesig.setup(bufnr, config)
-  hidesig.perform(bufnr, lang)
+  local config = ts_configs.get_module("hidesig")
+  local opacity = config.opacity
+  hidesig.fullUpdate(bufnr, lang, opacity)
+
+  cmd(string.format("augroup NvimHidesig_%d", bufnr))
+  cmd("au!")
+  for _, event in ipairs(cmdEvents) do
+    cmd(partialUpdateCmd(
+      event,
+      bufnr,
+      lang,
+      opacity
+    ))
+  end
+  cmd("augroup END")
 end
 
 function M.detach(bufnr)
   hidesig.teardown(bufnr)
+  --
+  for _, event in ipairs(cmdEvents) do
+    cmd(string.format("autocmd! NvimHidesig_%d %s", bufnr, event))
+  end
 end
 
 return M
